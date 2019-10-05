31670458432932b8619ac7f56dcc94bf5b86a9b5
Kshitiz Vijayvargiya <kshitiz.v@timesinternet.in>
2019-10-06 03:48:48 +0530

    initial commit

---
+++ example/index.js
@@ -0,0 +1,21 @@
+import { DfpAdItem } from '../src/index';
+import ReactDOM from 'react-dom';
+import React from 'react';
+const wrapper = document.getElementById('container');
+wrapper
+  ? ReactDOM.render(
+      <div>
+        <DfpAdItem
+          adUnitPath="/6355419/Travel/Europe/France/Paris"
+          size={[300, 250]}
+          elementId="banner1"
+        />
+        <DfpAdItem
+          adUnitPath="/6355419/Travel/Europe/France/Paris"
+          size={[300, 250]}
+          elementId="banner2"
+        />
+      </div>,
+      wrapper,
+    )
+  : false;
---
+++ src/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+function addtoList(adUnitPath, size, elementId) {
+  window.react_gpt_ads_list = Array.isArray(window.react_gpt_ads_list)
+    ? window.react_gpt_ads_list
+    : [];
+  window.react_gpt_ads_list.push({
+    adUnitPath,
+    size,
+    elementId,
+  });
+}
+function getList() {
+  return window.react_gpt_ads_list;
+}
+function emptyList() {
+  window.react_gpt_ads_list = [];
+}
+export function displayAd(list) {
+  const react_gpt_ads_list = Array.isArray(list) ? list : getList();
+  if (!Array.isArray(react_gpt_ads_list)) {
+    return;
+  }
+  window.googletag = window.googletag || {};
+  window.googletag.cmd = window.googletag.cmd || [];
+  const { googletag } = window;
+  googletag.cmd.push(() => {
+    react_gpt_ads_list.forEach(adItem => {
+      const { adUnitPath, size, elementId } = adItem;
+      const adCodeObj = googletag.defineSlot(adUnitPath, size, elementId);
+      adCodeObj.addService(googletag.pubads());
+    });
+
+    googletag.pubads().enableSingleRequest();
+    googletag.pubads().enableAsyncRendering();
+    googletag.pubads().collapseEmptyDivs(true);
+    googletag.enableServices();
+    react_gpt_ads_list.forEach(adItem => {
+      googletag.display(adItem.elementId);
+    });
+    emptyList();
+  });
+}
+
+export class DfpAdItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    const { adUnitPath, size, elementId } = this.props;
+    addtoList(adUnitPath, size, elementId);
+    setTimeout(() => {
+      displayAd();
+    }, 2000);
+  }
+  render() {
+    const { adUnitPath, size, elementId } = this.props;
+    return <div id={elementId}></div>;
+  }
+}
